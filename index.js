495eba8484f2d1b2ec70893ff9c65a7deb21fa9b
gilbert <eleazarbiboum@gmail.com>
2022-03-26 21:22:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// code your solution in this file
+const array = ["", "",""]
+const surprise = ""
+
+function writeCards(array, surprise) {
+
+  for (let i = 0; i < array.length;){
+     array = [`Thank you, ${array[i++]}, for the wonderful surprise gift!`, `Thank you, ${array[i++]}, for the wonderful surprise gift!`, `Thank you, ${array[i++]}, for the wonderful surprise gift!`];
+     debugger;
+  }
+return array;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+function countDown(){
+  let j = 0;
+  while(j < 11 ) {
+    console.log(j++);
+  }
+}
+
+countDown();
+
